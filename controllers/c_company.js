6e62c482fbc2d2a21d63a2a2c3b5352f1ff40ceb
Khai Hoang <khaihoangdev@gmail.com>
2018-01-19 10:19:38 +0700

    ....

--- controllers/c_company.js
+++ controllers/c_company.js
@@ -6,9 +6,26 @@ let COMPANY = mongoose.model('Company');
 
 
 let add = (company)=>{
-    let new_company = new COMPANY(company);
-    new_company.save().then((res)=>{
-        console.log(res);
+    let check = new COMPANY();
+    check.findOne({phone:company.phone}).then(res=>{
+        if(!res){
+            let new_company = new COMPANY(company);
+            new_company.save().then((res)=>{
+                console.log(res);
+            })
+        }
+    })
+
+
+}
+let update = (_company,_state,cb)=>{
+    let update_company = new COMPANY();
+    update_company.findByIdAndUpdate(_company,{state:_state},(err,res)=>{
+        if(!err){
+            return cb(null,res);
+        }else{
+            return cb(err,null);
+        }
     })
 }
 
