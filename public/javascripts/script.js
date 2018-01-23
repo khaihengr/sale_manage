14f2a295e9019e94fa6e255e2aa0d6dc3fd8cc22
Khai Hoang <khaihoangdev@gmail.com>
2018-01-23 15:17:40 +0700

    port update

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,23 +3,22 @@ $(document).ready(function (event) {
         $("#register_form").toggleClass('d-none','d-block');
     })
     $(".c_state").click(function() {
-            if ($(this).hasClass('btn-outline-dark')) {
-                $(this).removeClass('btn-outline-dark').addClass('btn-' + $(this).attr('value'));
-                $(this).siblings().attr('disabled', true)
-            } else {
-                $(this).siblings().attr('disabled', false)
-                $(this).removeClass('btn-' + $(this).attr('value')).addClass('btn-outline-dark');
-            }
-            var _state = $(this).attr('value');
-            $(this).closest('.col-5').find('.child-hidden').attr('value', _state);
-            $(this).closest('.col-5').find('form').submit();
-
+        if ($(this).hasClass('btn-outline-dark')) {
+            $(this).removeClass('btn-outline-dark').addClass('btn-' + $(this).attr('value'));
+            $(this).siblings().attr('disabled', true)
+        } else {
+            $(this).siblings().attr('disabled', false)
+            $(this).removeClass('btn-' + $(this).attr('value')).addClass('btn-outline-dark');
+        }
+        var _state = $(this).attr('value');
+        $(this).closest('.col-5').find('.child-hidden').attr('value', _state);
+        $(this).closest('.col-5').find('form').submit();
 
     })
     $("#_filter").change(()=>{
         $('#f_filter').submit();
     })
-    // $('._note').on('change', function () {
-    //     $(this).parent().submit();
+    // $('._note').focusout(()=>{
+    //        $(this).parent().submit();
     // })
 })
